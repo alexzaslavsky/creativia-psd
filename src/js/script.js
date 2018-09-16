4323ce52ad67bffbe19eb13512394b8b9d8c6b68
Alex Zaslavsky <alex.zaslavsky.ua@gmail.com>
2018-09-16 14:31:00 +0300

    button-fix

--- src/js/script.js
+++ src/js/script.js
@@ -8,12 +8,15 @@ window.onload = function() {
 
     function popMenu() {
         for (var i = 0; i < blocks.length; i++) {
-
-            blocks[i].classList.remove('plus-minus-ver-hidden');
-            blocks[i].classList.remove('sub-info-block-showed');
+            if (blocks[i] !== this) {
+                blocks[i].classList.remove('plus-minus-ver-hidden');
+                blocks[i].classList.remove('sub-info-block-showed');
+            }
         }
         this.classList.toggle('plus-minus-ver-hidden');
         this.classList.toggle('sub-info-block-showed');
+
+
     }
 
 };
